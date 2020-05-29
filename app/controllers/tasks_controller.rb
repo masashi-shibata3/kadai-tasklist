8ab508b9fed40aca4c0b1a4a0f9fe051fff8f781
taro.kirameki <taro.kirameki@techacademy.jp>
2020-05-29 06:25:45 +0000

    restart

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,54 @@
+class TasksController < ApplicationController
+  
+before_action :set_task, only: [:show, :edit, :update, :destroy]
+  def index
+      @tasks = Task.all.page(params[:page])
+  end
+  
+  def show
+  end
+
+  def new
+      @task = Task.new
+  end
+
+  def create
+      @task = Task.new(task_params)
+     
+      if @task.save
+        flash[:success] = 'タスクが投稿されました'
+        redirect_to @task
+      else
+        flash.now[:danger] = 'タスクが投稿されませんでした'
+        render :new
+      end
+  end
+
+  def edit
+  end
+
+  def update
+      if @task.update(task_params)
+         flash[:success]='タスクが編集されました'
+         redirect_to @task
+      else
+         flash.now[:danger] ='タスクが編集されませんでした'
+         render :new
+      end
+  end
+
+  def destroy
+      @task.destroy
+      
+      flash[:success] = 'タスクを削除しました'
+      redirect_to tasks_url
+  end
+  
+private
+   def set_task
+    @task = Task.find(params[:id])
+   end
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
+end
