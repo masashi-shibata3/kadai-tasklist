16e721d9170165f4f8dd39d91a6e398efa532544
taro.kirameki <taro.kirameki@techacademy.jp>
2020-06-03 08:33:40 +0000

    reset

--- app/controllers/toppages_controller.rb
+++
@@ -1,8 +0,0 @@
-class ToppagesController < ApplicationController
-  def index
-     if logged_in?
-      @task = current_user.tasklists.build  
-      @tasks = current_user.tasklists.order(id: :desc).page(params[:page])
-     end
-  end
-end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,6 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in
    
-  def index
-     @users = User.order(id: :desc).page(params[:page]).per(25)
-  end
-
-  def show
-  end
 
   def new
     @user = User.new
