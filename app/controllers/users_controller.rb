b7fe6d8802c8247b7561a78991ab8ac7dd62a076
taro.kirameki <taro.kirameki@techacademy.jp>
2020-06-04 01:28:40 +0000

    shuusei

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in
+  before_action :require_user_logged_in, only: [:show]
    
+  def show
+  end
 
   def new
     @user = User.new
