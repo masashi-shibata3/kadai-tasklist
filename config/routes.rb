50ca25d487e13326fd5480cfe31d2dde44c430c2
taro.kirameki <taro.kirameki@techacademy.jp>
2020-06-05 09:57:42 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
     
-    get 'signup', to: 'users#new'
-    resources :users, only: [ :show, :new, :create]
-    resources :tasks
+  get 'signup', to: 'users#new'
+  resources :users
+  resources :tasks
 end
