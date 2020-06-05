c2406d1cbec0fb3fe8934eb074930629b6728096
taro.kirameki <taro.kirameki@techacademy.jp>
2020-06-05 08:48:27 +0000

    heroku

--- db/migrate/20200530074638_add_users_to_tasks.rb
+++
@@ -1,5 +0,0 @@
-class AddUsersToTasks < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :tasks, :users, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_30_074638) do
+ActiveRecord::Schema.define(version: 2020_06_05_082038) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2020_05_30_074638) do
     t.datetime "updated_at", null: false
     t.string "status"
     t.bigint "user_id"
-    t.index ["user_id"], name: "index_tasks_on_users_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
